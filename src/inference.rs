63ed0b13e36ab5ad7b5c4a028b668e43da204780
Xinyu Bao <baoxinyuworks@163.com>
2025-02-21 08:58:30 +0000

    working on inference struct

--- src/inference.rs
+++ src/inference.rs
@@ -4,8 +4,28 @@ use async_openai::{config::OpenAIConfig, Client};
 
 use crate::graph::Graph;
 
-/// LLM 
+/// LLM inference with tree of thoughts
 pub struct Inference {
     graph: Arc<Mutex<Graph>>,
     client: Client<OpenAIConfig>
+}
+
+impl Inference {
+    pub fn new(
+        api_url: String, 
+        api_key: String
+    ) -> Self {
+        let client = Client::with_config(
+            OpenAIConfig::new()
+                .with_api_base(api_url)
+                .with_api_key(api_key)
+        );
+
+        Self {
+            graph: Arc::new(Mutex::new(Graph::new())), 
+            client: client
+        }
+    }
+
+    pub fn generate(&self, query: String) -> String {}
 }
